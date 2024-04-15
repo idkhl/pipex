9d17f1e7583df8aaee7b4c744027304914daa305
Ines Dakhlaoui <idakhlao@bess-f2r2s4.clusters.42paris.fr>
2024-04-15 17:50:45 +0200

    parsing ~finished

--- src/exec.c
+++ src/exec.c
@@ -1,26 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: idakhlao <idakhlao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 18:13:00 by idakhlao          #+#    #+#             */
-/*   Updated: 2024/04/15 17:04:56 by idakhlao         ###   ########.fr       */
+/*   Created: 2024/04/15 11:18:21 by idakhlao          #+#    #+#             */
+/*   Updated: 2024/04/15 12:30:47 by idakhlao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../includes/pipex.h"
 
-# include "../includes/libft.h"
-# include "../includes/ft_printf.h"
-# include <errno.h>
-# include <fcntl.h>
-
-typedef  struct s_pipex
-{
-	
-} t_pipex;
-
-#endif
--- src/main.c
+++ src/main.c
@@ -6,26 +6,35 @@
 /*   By: idakhlao <idakhlao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 18:18:08 by idakhlao          #+#    #+#             */
-/*   Updated: 2024/04/13 15:22:53 by idakhlao         ###   ########.fr       */
+/*   Updated: 2024/04/15 17:36:40 by idakhlao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	parse_file1(char	**file)
+void	wrong_args(int nb)
 {
-	if (access(file, R_OK) == -1)
-		return (-1);
-	return (0);
+	if (nb == 1)
+		ft_printf("FORMAT SHOULD BE [./pipex file1 cmd1 cmd2 file2]\n");
+	if (nb == 2)
+		ft_printf("COMMAND DOES NOT EXIST\n");
+	if (nb == 3)
+		ft_printf("FILE1 DOES NOT EXIST\n");
 }
 
-int	parse_cmd(char	**cmd)
+int	parse_cmd(char **arg)
 {
 	char	*bin;
 	char	*path;
+	char	**cmd;
 
+	cmd = ft_split(*arg, ' ');
+	if (!cmd)
+		return (-1);
+	printf("%s\n", cmd[0]);
 	bin = "/bin/";
-	path = ft_strjoin(bin, cmd[1]);
+	path = ft_strjoin(bin, cmd[0]);
+	malloc_free(cmd);
 	if (!path)
 		return (0);
 	if (access(path, R_OK) == -1)
@@ -34,22 +43,16 @@ int	parse_cmd(char	**cmd)
 		return (-1);
 	}
 	free(path);
+	return (0);
 }
 
 int	parsing(char **av)
 {
-	char	**cmd1;
-
-	cmd1 = av[2];
-	parsecmd(cmd1);
-}
-
-void	wrong_args(int nb)
-{
-	if (nb == 1)
-		ft_printf("FORMAT SHOULD BE [./pipex file1 cmd1 cmd2 file2]\n");
-	if (nb == 2)
-		ft_printf("COMMAND DOES NOT EXIST\n");
+	if (access(av[1], R_OK) == -1)
+		return (wrong_args(3), -1);
+	if (parse_cmd(&av[2]) == -1 || parse_cmd(&av[3]) == -1)
+		return (wrong_args(2), -1);
+	return (0);
 }
 
 int	main(int ac, char **av)
@@ -57,6 +60,7 @@ int	main(int ac, char **av)
 	if (ac != 5 || !av)
 		return (wrong_args(1), -1);
 	if (parsing(av) == -1)
-		return (wrong_args(2), -1);
+		return (-1);
+	// init_tab(av);
 	return (0);
 }
